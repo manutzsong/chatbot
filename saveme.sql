0cce3f71263ac38a54d9bf8de4547fb6ade68844
Manutzsong <manutzsong@gmail.com>
2018-01-09 22:31:33 +0700

    finish join event

--- saveme.sql
+++ saveme.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost:3306
--- Generation Time: Jan 06, 2018 at 05:38 AM
+-- Generation Time: Jan 09, 2018 at 03:30 PM
 -- Server version: 5.7.20-0ubuntu0.17.10.1
 -- PHP Version: 7.1.11-0ubuntu0.17.10.1
 
@@ -39,21 +39,51 @@ CREATE TABLE `intent` (
 --
 
 INSERT INTO `intent` (`id`, `intent`, `intent_id`, `time`, `last_see`) VALUES
-(51, 'smalltalk.greetings.hello', 'smalltalk.greetings.hello', 27, '2018-01-06 05:35:15'),
+(51, 'smalltalk.greetings.hello', 'smalltalk.greetings.hello', 42, '2018-01-09 15:13:44'),
 (52, 'BG2000', '095bf114-e852-42a2-823d-664ef3402952', 3, '2018-01-06 05:34:50'),
 (53, 'GE1403', '485e8503-ce4b-4b8f-af4c-a09dcc39df47', 0, '2017-12-30 11:55:59'),
-(55, 'Abac-ABAC-Info', '0ccda6b1-9314-4fb2-8b81-52a13d85d3b6', 8, '2018-01-06 05:34:59'),
+(55, 'Abac-ABAC-Info', '0ccda6b1-9314-4fb2-8b81-52a13d85d3b6', 11, '2018-01-09 04:38:15'),
 (57, 'BG1001', 'e8f148eb-d965-4879-80e9-c1cb97ffe8dd', 0, '2017-12-30 15:56:33'),
 (58, 'BG1002', 'db1c4dc0-453a-4201-93e3-bc2536dcae4b', 0, '2017-12-30 15:56:35'),
 (60, 'BG2001', 'e33ebcbc-db1f-406a-b465-e74dedbe48bc', 4, '2018-01-06 05:35:00'),
-(61, 'MIS1221', '6bb66f2f-9700-4565-9647-983a995b8287', 2, '2018-01-06 04:57:57'),
+(61, 'MIS1221', '6bb66f2f-9700-4565-9647-983a995b8287', 4, '2018-01-09 04:38:00'),
 (62, 'smalltalk.agent.chatbot', 'smalltalk.greetings.hello', 0, '2017-12-30 15:56:55'),
-(63, 'MIS3231', '6a5baec3-ebf6-4cd4-b378-95a47acde4a4', 3, '2018-01-06 05:10:01'),
-(67, 'Default Fallback Intent', 'e2677674-a800-4d50-8cde-ba341fb3c0a4', 23, '2018-01-06 05:10:53'),
-(74, 'smalltalk.user.testing_agent', 'smalltalk.greetings.hello', 0, '2018-01-04 12:30:20'),
+(63, 'MIS3231', '6a5baec3-ebf6-4cd4-b378-95a47acde4a4', 4, '2018-01-09 04:38:00'),
+(67, 'Default Fallback Intent', 'e2677674-a800-4d50-8cde-ba341fb3c0a4', 60, '2018-01-09 15:26:35'),
+(74, 'smalltalk.user.testing_agent', 'smalltalk.greetings.hello', 2, '2018-01-09 12:57:02'),
 (82, 'smalltalk.agent.can_you_help', 'smalltalk.greetings.hello', 1, '2018-01-05 04:12:39'),
 (111, 'smalltalk.agent.busy', 'smalltalk.greetings.hello', 0, '2018-01-06 04:58:05'),
-(117, 'MIS3221', '0b2d241b-e30c-4822-b2ca-f89795c05d2d', 1, '2018-01-06 05:09:56');
+(117, 'MIS3221', '0b2d241b-e30c-4822-b2ca-f89795c05d2d', 1, '2018-01-06 05:09:56'),
+(144, 'smalltalk.appraisal.well_done', 'smalltalk.greetings.hello', 1, '2018-01-09 04:50:13'),
+(147, 'smalltalk.confirmation.no', 'smalltalk.greetings.hello', 2, '2018-01-09 12:44:31'),
+(153, 'smalltalk.agent.sure', 'smalltalk.greetings.hello', 0, '2018-01-09 04:36:54'),
+(166, 'smalltalk.dialog.wrong', 'smalltalk.greetings.hello', 0, '2018-01-09 04:38:29'),
+(172, 'smalltalk.confirmation.yes', 'smalltalk.greetings.hello', 3, '2018-01-09 12:44:58'),
+(173, 'smalltalk.emotions.ha_ha', 'smalltalk.greetings.hello', 0, '2018-01-09 04:47:28'),
+(178, 'smalltalk.user.joking', 'smalltalk.greetings.hello', 0, '2018-01-09 04:47:42'),
+(185, 'smalltalk.agent.occupation', 'smalltalk.greetings.hello', 0, '2018-01-09 04:50:18');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `line_user`
+--
+
+CREATE TABLE `line_user` (
+  `id` int(11) NOT NULL,
+  `uid` varchar(100) NOT NULL,
+  `sex` char(1) DEFAULT NULL,
+  `year` char(1) DEFAULT NULL,
+  `major` varchar(50) DEFAULT NULL,
+  `date_create` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `line_user`
+--
+
+INSERT INTO `line_user` (`id`, `uid`, `sex`, `year`, `major`, `date_create`) VALUES
+(8, 'U2e78e9caf714536a98df758427f55917', '1', '4', 'bba', '2018-01-09 15:24:55');
 
 -- --------------------------------------------------------
 
@@ -75,8 +105,8 @@ CREATE TABLE `most` (
 --
 
 INSERT INTO `most` (`id`, `uid`, `prof_name`, `time_usage`, `last_see`, `date_create`) VALUES
-(19, 'U2e78e9caf714536a98df758427f55917', '🎸Song-Purin ™👣', 105, '2018-01-06 05:35:15', '2017-12-29 16:53:45'),
-(26, '99999e78e9caf714536a98df758427f55917', NULL, 15, '2017-12-30 03:16:40', '2017-12-30 03:16:40'),
+(19, 'U2e78e9caf714536a98df758427f55917', '🎸Song-Purin ™👣', 187, '2018-01-09 15:27:54', '2017-12-29 16:53:45'),
+(26, '99999e78e9caf714536a98df758427f55917', '🎸', 65, '2017-12-30 03:16:40', '2017-12-30 03:16:40'),
 (35, '1111', '1111111111111111111', 0, '2017-12-30 04:57:07', '2017-12-30 04:57:07'),
 (67, 'U865ad72605e8f6bb2094401078056545', 'นุ่น bedroom', 9, '2018-01-04 12:30:35', '2018-01-04 12:29:52');
 
@@ -109,19 +139,7 @@ CREATE TABLE `mute` (
 --
 
 INSERT INTO `mute` (`id`, `group_id`, `date_create`) VALUES
-(14, 'Cab6a1cd6eb461a071939ca6f1770e58d', '2017-12-27 12:59:31'),
-(81, '125', '2018-01-04 12:21:22'),
-(82, '215215', '2018-01-04 12:21:22'),
-(83, '125', '2018-01-04 12:21:24'),
-(84, '215215', '2018-01-04 12:21:24'),
-(85, '125', '2018-01-04 12:21:28'),
-(86, '215215', '2018-01-04 12:21:28'),
-(87, '125', '2018-01-04 12:21:37'),
-(88, '215215', '2018-01-04 12:21:37'),
-(89, '125', '2018-01-04 12:21:39'),
-(90, '215215', '2018-01-04 12:21:39'),
-(91, '125', '2018-01-04 12:21:46'),
-(92, '215215', '2018-01-04 12:21:46');
+(14, 'Cab6a1cd6eb461a071939ca6f1770e58d', '2017-12-27 12:59:31');
 
 -- --------------------------------------------------------
 
@@ -187,7 +205,12 @@ INSERT INTO `today_intent` (`id`, `intent`, `intent_id`, `time`, `intent_day`, `
 (5, 'BG2001', 'e33ebcbc-db1f-406a-b465-e74dedbe48bc', 2, '2018-01-06', '2018-01-05 17:00:00'),
 (6, 'BG2000', '095bf114-e852-42a2-823d-664ef3402952', 1, '2018-01-06', '2018-01-06 05:34:50'),
 (7, 'Abac-ABAC-Info', '0ccda6b1-9314-4fb2-8b81-52a13d85d3b6', 1, '2018-01-06', '2018-01-06 05:34:59'),
-(9, 'smalltalk.greetings.hello', 'smalltalk.greetings.hello', 4, '2018-01-06', '2018-01-05 17:00:00');
+(9, 'smalltalk.greetings.hello', 'smalltalk.greetings.hello', 4, '2018-01-06', '2018-01-05 17:00:00'),
+(13, 'smalltalk.greetings.hello', 'smalltalk.greetings.hello', 31, '2018-01-09', '2018-01-08 17:00:00'),
+(15, 'Default Fallback Intent', 'e2677674-a800-4d50-8cde-ba341fb3c0a4', 37, '2018-01-09', '2018-01-08 17:00:00'),
+(25, 'MIS1221', '6bb66f2f-9700-4565-9647-983a995b8287', 2, '2018-01-09', '2018-01-08 17:00:00'),
+(27, 'Abac-ABAC-Info', '0ccda6b1-9314-4fb2-8b81-52a13d85d3b6', 3, '2018-01-09', '2018-01-08 17:00:00'),
+(37, 'MIS3231', '6a5baec3-ebf6-4cd4-b378-95a47acde4a4', 1, '2018-01-09', '2018-01-09 04:38:00');
 
 -- --------------------------------------------------------
 
@@ -214,7 +237,8 @@ INSERT INTO `traffic` (`id`, `count_traffic`, `date`) VALUES
 (38, 2, '2017-12-31'),
 (40, 25, '2018-01-04'),
 (65, 7, '2018-01-05'),
-(72, 40, '2018-01-06');
+(72, 44, '2018-01-06'),
+(116, 82, '2018-01-09');
 
 -- --------------------------------------------------------
 
@@ -246,6 +270,13 @@ INSERT INTO `user` (`id`, `username`, `password`, `last_login`) VALUES
 ALTER TABLE `intent`
   ADD PRIMARY KEY (`id`),
   ADD UNIQUE KEY `intent` (`intent`);
+
+--
+-- Indexes for table `line_user`
+--
+ALTER TABLE `line_user`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `uid` (`uid`);
 
 --
 -- Indexes for table `most`
@@ -294,12 +325,17 @@ ALTER TABLE `user`
 -- AUTO_INCREMENT for table `intent`
 --
 ALTER TABLE `intent`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=138;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=212;
+--
+-- AUTO_INCREMENT for table `line_user`
+--
+ALTER TABLE `line_user`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
 --
 -- AUTO_INCREMENT for table `most`
 --
 ALTER TABLE `most`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=134;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=216;
 --
 -- AUTO_INCREMENT for table `mute`
 --
@@ -314,12 +350,12 @@ ALTER TABLE `register`
 -- AUTO_INCREMENT for table `today_intent`
 --
 ALTER TABLE `today_intent`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=87;
 --
 -- AUTO_INCREMENT for table `traffic`
 --
 ALTER TABLE `traffic`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=112;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=198;
 --
 -- AUTO_INCREMENT for table `user`
 --
